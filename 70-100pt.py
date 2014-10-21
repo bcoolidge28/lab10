86113f163ff7c43b3208a57123b1a944b82ffba7
bcoolidge28 <s113256@student.roundrockisd.org>
2014-10-21 15:57:56 -0500

    almost done lab 10

--- 70-100pt.py
+++ 70-100pt.py
@@ -24,5 +24,25 @@ drawpad.grid(row=0, column=1)
 
 # Insert your code here to draw the house!
 
+#house outline
+rectangle = drawpad.create_rectangle(300,300,600,500, fill = 'red')
 
+line1 = drawpad.create_line(300,300,450,100)
+line2 = drawpad.create_line(600,300,450,100)
+
+#windows and door
+rectangle2 = drawpad.create_rectangle(350,350,400,400, fill = 'white')
+rectangle3 = drawpad.create_rectangle(500,350,550,400, fill = 'white')
+
+rectangle4 = drawpad.create_rectangle(425,350,475,500, fill = 'orange')
+
+#door handle
+oval1 = drawpad.create_oval(460,425,470,450, fill = 'yellow')
+
+line3 = drawpad.create_line(550,50,550,230)
+line4 = drawpad.create_line(575,50,575,270)
+line5 = drawpad.create_line(550,50,575,50)
+
+#grass
+rectangle5 = drawpad.create_rectangle(800,500,0,600, fill = 'green')
 root.mainloop()
